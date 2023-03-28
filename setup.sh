6735bd560e7ee8e7162b2071705d8663c2cddc69
tlepple <49572881+tlepple@users.noreply.github.com>
2023-03-28 07:16:23 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -255,7 +255,7 @@ chown datagen:datagen -R /home/datagen/
 ##########################################################################################
 #  pip install some items
 ##########################################################################################
-#sudo pip install kafka-python uuid simplejson faker psycopg2-binary pyiceberg pyarrow boto3
+sudo pip install kafka-python uuid simplejson faker psycopg2-binary pyiceberg pyarrow boto3
 #sudo /opt/jupyterhub/bin/python3 -m pip install kafka-python uuid simplejson faker psycopg2-binary pyiceberg pyarrow boto3
 echo
 echo "---------------------------------------------------------------------"
